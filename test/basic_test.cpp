d5dc3f6ee1d2bea5a023735eb3511871dea8e021
Jagirhussan <r.jagir@auckland.ac.nz>
2022-07-01 10:52:12 +1200

    Statically link dependencies 2 simplify pluginload

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -27,196 +27,189 @@ along with this program. If not, see <https://gnu.org/licenses>.
 using namespace BG;
 // Demonstrate some basic assertions.
 
-RCPLIB::RCP<BondGraphInterface> rlc()
-{
-    auto ioBondGraph = createBondGraph();
+RCPLIB::RCP<BondGraphInterface> rlc() {
+  auto ioBondGraph = createBondGraph();
 
-    //Create the storage
-    auto lI = createInductor();
-    ioBondGraph->addComponent(lI);
+  // Create the storage
+  auto lI = createInductor();
+  ioBondGraph->addComponent(lI);
 
-    auto lC1 = createCapacitor();
-    ioBondGraph->addComponent(lC1);
+  auto lC1 = createCapacitor();
+  ioBondGraph->addComponent(lC1);
 
-    //Create the resistor
-    auto lR = createResistor();
-    ioBondGraph->addComponent(lR);
+  // Create the resistor
+  auto lR = createResistor();
+  ioBondGraph->addComponent(lR);
 
-    auto lE = createConstantVoltageSource();
-    ioBondGraph->addComponent(lE);
+  auto lE = createConstantVoltageSource();
+  ioBondGraph->addComponent(lE);
 
-    //Create the junctions
-    auto lJ0_1 = createZeroJunction();
-    ioBondGraph->addComponent(lJ0_1);
+  // Create the junctions
+  auto lJ0_1 = createZeroJunction();
+  ioBondGraph->addComponent(lJ0_1);
 
-    //Create the bonds
-    ioBondGraph->connect(lR, lJ0_1);
-    ioBondGraph->connect(lI, lJ0_1);
-    ioBondGraph->connect(lC1, lJ0_1);
-    ioBondGraph->connect(lE, lJ0_1);
+  // Create the bonds
+  ioBondGraph->connect(lR, lJ0_1);
+  ioBondGraph->connect(lI, lJ0_1);
+  ioBondGraph->connect(lC1, lJ0_1);
+  ioBondGraph->connect(lE, lJ0_1);
 
-    return ioBondGraph;
+  return ioBondGraph;
 }
 
-RCPLIB::RCP<BondGraphInterface> reaction()
-{
-    auto ioBondGraph = createBondGraph();
+RCPLIB::RCP<BondGraphInterface> reaction() {
+  auto ioBondGraph = createBondGraph();
 
-    //Create the storage
-    auto A = createConcentration();
-    ioBondGraph->addComponent(A);
+  // Create the storage
+  auto A = createConcentration();
+  ioBondGraph->addComponent(A);
 
-    auto B = createConcentration();
-    ioBondGraph->addComponent(B);
+  auto B = createConcentration();
+  ioBondGraph->addComponent(B);
 
-    //Create the reaction
-    auto Re = createReaction();
-    ioBondGraph->addComponent(Re);
+  // Create the reaction
+  auto Re = createReaction();
+  ioBondGraph->addComponent(Re);
 
-    //Create the junctions
-    auto Y_A = createOneJunction();
-    ioBondGraph->addComponent(Y_A);
+  // Create the junctions
+  auto Y_A = createOneJunction();
+  ioBondGraph->addComponent(Y_A);
 
-    auto Y_B = createOneJunction();
-    ioBondGraph->addComponent(Y_B);
+  auto Y_B = createOneJunction();
+  ioBondGraph->addComponent(Y_B);
 
-    //Create the bonds
-    ioBondGraph->connect(A, Y_A);
-    ioBondGraph->connect(B, Y_B);
-    ioBondGraph->connectInverting(Re, 0, Y_A);
-    ioBondGraph->connectInverting(Re, 1, Y_B);
-    return ioBondGraph;
+  // Create the bonds
+  ioBondGraph->connect(A, Y_A);
+  ioBondGraph->connect(B, Y_B);
+  ioBondGraph->connectInverting(Re, 0, Y_A);
+  ioBondGraph->connectInverting(Re, 1, Y_B);
+  return ioBondGraph;
 }
 
-TEST(BondGraphSetup, Electrical)
-{
-    newWorkSpace();
-    // Expect two strings not to be equal.
-    auto ioBondGraph = rlc();
+TEST(BondGraphSetup, Electrical) {
+  newWorkSpace();
+  // Expect two strings not to be equal.
+  auto ioBondGraph = rlc();
 
-    auto res = ioBondGraph->computeStateEquation();
-    //Solvable
-    EXPECT_EQ(res.bondGraphValidity, true);
-    auto equations = res.dof;
-    std::map<std::string, std::string> result = {
-        {"q_1","u_3*C_1"},
-        {"dot_p_0","u_3"},
-        {"dot_q_1","C_1*dot_u_3"}
-    };
-    for (auto eq : equations) {
-        std::string svar = symEngineExpressionToString(eq.first);
-        std::string sres = symEngineExpressionToString(eq.second);
-        std::string expected = result[svar];
-        EXPECT_EQ(sres, expected);
-    }
+  auto res = ioBondGraph->computeStateEquation();
+  // Solvable
+  EXPECT_EQ(res.bondGraphValidity, true);
+  auto equations = res.dof;
+  std::map<std::string, std::string> result = {
+      {"q_1", "u_3*C_1"},
+      {"dot_Inductor", "u_3"},
+      {"dot_Capacitor", "C_1*dot_u_3"}};
+
+  for (auto eq : equations) {
+    std::string svar = symEngineExpressionToString(eq.first);
+    std::string sres = symEngineExpressionToString(eq.second);
+    std::string expected = result[svar];
+    EXPECT_EQ(sres, expected);
+  }
 }
 
-TEST(BondGraphSetup, Biochemical)
-{
-    newWorkSpace();
-    auto ioBondGraph = reaction();
+TEST(BondGraphSetup, Biochemical) {
+  newWorkSpace();
+  auto ioBondGraph = reaction();
 
-    auto res = ioBondGraph->computeStateEquation();
-    //Solvable
-    EXPECT_EQ(res.bondGraphValidity, true);
-    auto equations = res.dof;
-    std::map<std::string, std::string> result = {
-        {"dot_a_0" , "-(k*a_0*r_2 - k*a_1*r_2)"},
-        {"dot_a_1" , "-(-k*a_0*r_2 + k*a_1*r_2)"}};
-    for (auto eq : equations) {
-        std::string svar = symEngineExpressionToString(eq.first);
-        std::string sres = symEngineExpressionToString(eq.second);
-        std::string expected = result[svar];
-        EXPECT_EQ(sres, expected);
-    }
+  auto res = ioBondGraph->computeStateEquation();
+  // Solvable
+  EXPECT_EQ(res.bondGraphValidity, true);
+  auto equations = res.dof;
+  std::map<std::string, std::string> result = {
+      {"dot_Ce", "-Ce*r_2*k_0 + Ce*r_2*k_1"}};
+
+  for (auto eq : equations) {
+    std::string svar = symEngineExpressionToString(eq.first);
+    std::string sres = symEngineExpressionToString(eq.second);
+    std::string expected = result[svar];
+    EXPECT_EQ(sres, expected);
+  }
 }
 
-TEST(BondGraphSetup, Composition)
-{
-    newWorkSpace();
-    auto bg = reaction();
-    auto parent = createBondGraph();
-    parent->addBondGraph(bg);
-    auto clone = bg->clone();
-    parent->addBondGraph(clone);
-    parent->computeStateEquation();
-    auto res = parent->computeStateEquation();
-    //Solvable
-    EXPECT_EQ(res.bondGraphValidity, true);
-    auto equations = res.dof;
-    std::map<std::string, std::string> result = {
-        {"dot_a_0","-(k*a_0*r_5 - k*a_1*r_5)"},
-        {"dot_a_1","-(-k*a_0*r_5 + k*a_1*r_5)"},
-        {"dot_a_2","-(k*a_2*r_5 - k*a_3*r_5)"},
-        {"dot_a_3","-(-k*a_2*r_5 + k*a_3*r_5)"}
-    };
+TEST(BondGraphSetup, Composition) {
+  newWorkSpace();
+  auto bg = reaction();
+  auto parent = createBondGraph();
+  parent->addBondGraph(bg);
+  auto clone = bg->clone();
+  parent->addBondGraph(clone);
+  parent->computeStateEquation();
+  auto res = parent->computeStateEquation();
+  // Solvable
+  EXPECT_EQ(res.bondGraphValidity, true);
+  auto equations = res.dof;
+  std::map<std::string, std::string> result = {
+      {"dot_Ce", "-Ce*r_5*k_2 + Ce*r_5*k_3"},
+      {"dot_a_1", "-(-k*a_0*r_5 + k*a_1*r_5)"},
+      {"dot_a_2", "-(k*a_2*r_5 - k*a_3*r_5)"},
+      {"dot_a_3", "-(-k*a_2*r_5 + k*a_3*r_5)"}};
 
-    for (auto eq : equations) {
-        std::string svar = symEngineExpressionToString(eq.first);
-        std::string sres = symEngineExpressionToString(eq.second);
-        std::string expected = result[svar];
-        EXPECT_EQ(sres, expected);
-    }
+  for (auto eq : equations) {
+    std::string svar = symEngineExpressionToString(eq.first);
+    std::string sres = symEngineExpressionToString(eq.second);
+    std::string expected = result[svar];
+    EXPECT_EQ(sres, expected);
+  }
 }
 
-TEST(BondGraphSetup, Serialisation)
-{
-    newWorkSpace();
-    auto bg = reaction();
-    auto parent = createBondGraph();
-    parent->addBondGraph(bg);
-    auto clone = bg->clone();
-    clone->computeStateEquation();
-    parent->addBondGraph(clone);
+TEST(BondGraphSetup, Serialisation) {
+  newWorkSpace();
+  auto bg = reaction();
+  auto parent = createBondGraph();
+  parent->addBondGraph(bg);
+  auto clone = bg->clone();
+  clone->computeStateEquation();
+  parent->addBondGraph(clone);
 
-    auto eqs = bg->computeStateEquation();
-    auto files = getCellML("Reaction", bg, eqs);
-    for(const auto& itm : files){
-        std::string fname = "D:/Temp/"+itm.first;
-        std::ofstream ofs(fname.c_str(),std::ofstream::out);
-        ofs<<itm.second;
-        ofs.close();
-    }
+  auto eqs = bg->computeStateEquation();
+  auto files = getCellML("Reaction", bg, eqs);
+  for (const auto &itm : files) {
+    std::string fname = "D:/Temp/" + itm.first;
+    std::ofstream ofs(fname.c_str(), std::ofstream::out);
+    ofs << itm.second;
+    ofs.close();
+  }
 
-    nlohmann::json j = parent;
+  nlohmann::json j = parent;
 
-    newWorkSpace();
+  newWorkSpace();
 
-    RCPLIB::RCP<BondGraphInterface> bg2 = createBondGraph();
-    j.get_to(bg2);
+  RCPLIB::RCP<BondGraphInterface> bg2 = createBondGraph();
+  j.get_to(bg2);
 
-    nlohmann::json j2 = bg2;
-    EXPECT_EQ((j == j2),true);
+  nlohmann::json j2 = bg2;
+  EXPECT_EQ((j == j2), true);
 }
 
-TEST(BondGraphSetup,CellML){
-    newWorkSpace();
-    auto ioBondGraph = createBondGraph();
+TEST(BondGraphSetup, CellML) {
+  newWorkSpace();
+  auto ioBondGraph = createBondGraph();
 
-    //Create the resistor
-    auto lR = createResistor();
-    lR->setParameter("r", "1", "Ohm");
-    ioBondGraph->addComponent(lR);
+  // Create the resistor
+  auto lR = createResistor();
+  lR->setParameter("r", "1", "Ohm");
+  ioBondGraph->addComponent(lR);
 
-    auto lC1 = createCapacitor();
-    lC1->setParameter("C", "1", "Farad");
-    lC1->setParameter("q_0", "0", "coulomb");
-    ioBondGraph->addComponent(lC1);
+  auto lC1 = createCapacitor();
+  lC1->setParameter("C", "1", "Farad");
+  lC1->setParameter("q_0", "0", "coulomb");
+  ioBondGraph->addComponent(lC1);
 
-    //Create the junctions
-    auto lJ0_1 = createOneJunction();
-    ioBondGraph->addComponent(lJ0_1);
+  // Create the junctions
+  auto lJ0_1 = createOneJunction();
+  ioBondGraph->addComponent(lJ0_1);
 
-    //Create the bonds
-    ioBondGraph->connect(lR, lJ0_1);
-    ioBondGraph->connect(lC1, lJ0_1);
+  // Create the bonds
+  ioBondGraph->connect(lR, lJ0_1);
+  ioBondGraph->connect(lC1, lJ0_1);
 
-    auto eqs = ioBondGraph->computeStateEquation();
-    auto files = getCellML("RLC", ioBondGraph, eqs);
+  auto eqs = ioBondGraph->computeStateEquation();
+  auto files = getCellML("RLC", ioBondGraph, eqs);
 }
 
-TEST(BondGraphSetup,SupportedDomains){
-    newWorkSpace();
-    auto sd = getSupportedPhysicalDomainsAndFactoryMethods();
-    EXPECT_EQ(true,sd.contains("ElementDefinitions"));
+TEST(BondGraphSetup, SupportedDomains) {
+  newWorkSpace();
+  auto sd = getSupportedPhysicalDomainsAndFactoryMethods();
+  EXPECT_EQ(true, sd.contains("ElementDefinitions"));
 }
